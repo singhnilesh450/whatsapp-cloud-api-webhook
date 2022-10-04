a54f9ce514268711fd8e4cf1da40d58f7e6ddd77
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2022-10-05 00:07:50 +0530

    dj

--- index.js
+++ index.js
@@ -20,14 +20,17 @@ app.get("/webhook", (req, res) => {
   let mode = req.query["hub.mode"];
   let challange = req.query["hub.challenge"];
   let token = req.query["hub.verify_token"];
-
-  if (mode && token) {
-    if (mode === "subscribe" && token === mytoken) {
-      res.status(200).send(challange);
-    } else {
-      res.status(403);
-    }
-  }
+  res.status("200").json({
+    status: "100",
+    token: "token",
+  });
+  // if (mode && token) {
+  //   if (mode === "subscribe" && token === mytoken) {
+  //     res.status(200).send(challange);
+  //   } else {
+  //     res.status(403);
+  //   }
+  // }
 });
 
 app.post("/webhook", (req, res) => {
